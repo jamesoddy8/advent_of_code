aba9c1a5624f4c29955f19d65d2b738de7a264fa
James Oddy <jamesoddy8@gmail.com>
2020-12-05 18:37:50 +0000

    starting day 5

---
+++ day_5/task5.rb
@@ -0,0 +1,19 @@
+# def with input (ticket)
+# binary search tree?
+
+class Node
+  attr_reader :value
+  attr_accessor :left, :right
+
+
+  def initialize(value)
+    @value = value
+    @left =nil
+    @right =nil
+"F" == left
+"B" == right
+"R" == right
+"L" == left
+end
+end
+puts array
