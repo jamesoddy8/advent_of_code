b66635324be39465f49444ff7707a5916fccf104
James Oddy <jamesoddy8@gmail.com>
2020-12-06 23:20:49 +0000

    step 2 solved

--- day_6/task6.rb
+++ day_6/task6.rb
@@ -4,5 +4,13 @@ input = File.read(file_path)
 
 groups = input.split("\n\n")
 
+
 puts groups.map{ |votes| votes.gsub(/\s/, '').chars.uniq.count }
-.sum 
+.sum
+puts groups.map{ |votes|
+  everyone = votes.gsub(/\s/, '').chars.uniq
+
+  everyone.count do |x|
+    votes.split("\n").all? { |line| line.include?(x) }
+  end
+}.sum
